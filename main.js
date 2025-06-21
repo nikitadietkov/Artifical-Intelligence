a106f3e0c8625d492dc2075f211f49b4221e5f02
nikitadietkov <detkovnikita005@gmail.com>
2025-06-21 23:18:41 +0300

    header working)

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 $(document).ready(function () {
-    $(document).on('click', '#menuBtn', function () {
-        $('.phoneNavigation').addClass('openNav');
-    });
-    $(document).on('click', '.phoneNavigation', function () {
-        $('.phoneNavigation').removeClass('openNav');
-    });
+    showAndHide('nav', '.phoneNavigation', 'openNav')
+    showAndHide('#playVideo', '.video', 'videoShow')
 });
+
+function showAndHide(triggerSelector, targetSelector, klass) {
+    $(document).on('click', triggerSelector, function (e) {
+        e.stopPropagation();
+        $(targetSelector).show(0, function () {
+            $(targetSelector).animate({ opacity: 1 }, 300)
+        });
+    });
+    $(document).on('click', targetSelector, function (e) {
+        e.stopPropagation();
+    });
+    $(document).on('click', function () {
+        $(targetSelector).animate({opacity: 0}, 300, function () {
+            $(targetSelector).hide(0)
+        });
+    });
+    $(document).on('click', `${targetSelector} p`, function () {
+        $(targetSelector).animate({ opacity: 0 }, 300, function () {
+            $(targetSelector).hide(0)
+        });
+    });
+    $(document).on('click', `${targetSelector} button`, function () {
+        $(targetSelector).animate({ opacity: 0 }, 300, function () {
+            $(targetSelector).hide(0)
+        });
+    });
+}
