32777ac20a550e76f0a3568bfacf59aee00e89a3
nikitadietkov <detkovnikita005@gmail.com>
2025-06-23 17:55:20 +0300

    future added

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
     showAndHide('nav', '.phoneNavigation', 'openNav')
     showAndHide('#playVideo', '.video', 'videoShow')
+
+
+
+    let perPageCount = 0;
+    if (window.innerWidth >= 700) {
+        perPageCount = 5;
+    } else {
+        perPageCount = 2 
+    }
+
+    var splide = new Splide('.splide', {
+        type: 'loop',
+        perPage: perPageCount,
+        perMove: 1,
+    });
+
+    splide.mount();
 });
 
 function showAndHide(triggerSelector, targetSelector, klass) {
