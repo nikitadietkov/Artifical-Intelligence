cdd4a1ec239d715424e81d8bb83f2f5127b260ab
nikitadietkov <detkovnikita005@gmail.com>
2025-06-23 12:26:04 +0300

    header

--- main.js
+++ main.js
@@ -6,21 +6,15 @@ $(document).ready(function () {
 function showAndHide(triggerSelector, targetSelector, klass) {
     $(document).on('click', triggerSelector, function (e) {
         e.stopPropagation();
-        $(targetSelector).show(0, function () {
-            $(targetSelector).animate({ opacity: 1 }, 300)
-        });
+        $(targetSelector).addClass(klass)
     });
     $(document).on('click', targetSelector, function (e) {
         e.stopPropagation();
     });
     $(document).on('click', function () {
-        $(targetSelector).animate({opacity: 0}, 300, function () {
-            $(targetSelector).hide(0)
-        });
+        $(targetSelector).removeClass(klass);
     });
     $(document).on('click', `${targetSelector} p, ${targetSelector} button`, function () {
-        $(targetSelector).animate({ opacity: 0 }, 300, function () {
-            $(targetSelector).hide(0)
-        });
+        $(targetSelector).removeClass(klass);
     });
 }
